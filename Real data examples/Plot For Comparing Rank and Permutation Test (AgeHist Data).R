b788d1b77195ab248a153a0293bd560206f5e969
vidazamani <53086022+vidazamani@users.noreply.github.com>
2026-06-03 17:31:26 +0300

    Update Plot For Comparing Rank and Permutation Test (AgeHist Data).R

--- Real data examples/Plot For Comparing Rank and Permutation Test (AgeHist Data).R
+++ Real data examples/Plot For Comparing Rank and Permutation Test (AgeHist Data).R
@@ -38,6 +38,12 @@ df_all_tests$Test <- factor(df_all_tests$Test,
                                        "K-type",
                                        "H-type"))
 
+
+
+
+df_all_tests_noK <- df_all_tests %>%
+  filter(Test != "K-type")
+
 ##### Plot with logarithmic y axis 
 
 p_all_tests <- ggplot(df_all_tests,
@@ -75,12 +81,47 @@ p_all_tests <- ggplot(df_all_tests,
   )
 
 
+p_all_tests_noK <- ggplot(df_all_tests_noK,
+                      aes(x = Model,
+                          y = logP,
+                          fill = Test)) +
+  
+  geom_col(position = position_dodge(width = 0.75),
+           width = 0.65,
+           color = "black",
+           linewidth = 0.4) +
+  
+  geom_hline(yintercept = -log10(0.05),
+             linetype = "dashed",
+             linewidth = 0.8,
+             color = "blue") +
+  
+  scale_fill_manual(values = c("Permutation" = "grey20",
+                               "K-type"     = "grey50",
+                               "H-type"     = "grey80")) +
+  
+  facet_wrap(~ Scenario, nrow = 1) +
+  
+  labs(
+    x = "",
+    y = expression(-log[10](italic(p_value))),
+    fill = ""
+  ) +
+  
+  theme_classic(base_size = 12) +
+  theme(
+    legend.position = "bottom",
+    axis.text.x = element_text(angle = 30, hjust = 1),
+    panel.grid = element_blank()
+  )
+
+
 ##### Plot without logarithmic y axis 
 
 p_all_tests_o <- ggplot(df_all_tests,
-                      aes(x = Model,
-                          y = PValue,
-                          fill = Test)) +
+                        aes(x = Model,
+                            y = PValue,
+                            fill = Test)) +
   
   geom_col(position = position_dodge(width = 0.75),
            width = 0.65,
@@ -114,4 +155,3 @@ p_all_tests_o <- ggplot(df_all_tests,
 
 print(p_all_tests)
 print(p_all_tests_o)
-
